a61cd9c3fe4609b2274c7570ad1354b15f90f18c
Admin <kusanovbayastan@gmail.com>
2024-04-23 01:44:31 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tigers_journey_evolution/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tigers_journey_evolution
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runZonedGuarded(
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      runApp(
+        ScreenUtilInit(
+          designSize: const Size(393, 852),
+          builder: (context, child) {
+            return const MyApp();
+          },
+        ),
+      );
+    },
+    (error, stack) {
+      debugPrint(error.toString());
+      debugPrintStack(stackTrace: stack);
+    },
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
